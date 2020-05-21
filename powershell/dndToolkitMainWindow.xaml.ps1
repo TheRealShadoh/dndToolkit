6317aaaeb1f0ea2a39a1606a385f59893d07c89a
therealshadoh <christian.crill@outlook.com>
2020-05-21 02:48:15 -0400

    wired tab swapping up

--- powershell/dndToolkitMainWindow.xaml.ps1
+++ powershell/dndToolkitMainWindow.xaml.ps1
@@ -1,14 +1,18 @@
 ﻿Add-Type -AssemblyName presentationframework
+Add-Type -AssemblyName System.Windows.Forms
 
 function Add-ControlVariables { # May need to add variables from UI here manually to call them
 
 New-Variable -Name 'window' -Value $window.FindName('window') -Scope 1 -Force	
-New-Variable -Name 'playerButton' -Value $window.FindName('playerButton') -Scope 1 -Force	
 New-Variable -Name 'playerPicture' -Value $window.FindName('playerPicture') -Scope 1 -Force
 New-Variable -Name 'languageComboBox' -Value $window.FindName('languageComboBox') -Scope 1 -Force	
 New-Variable -Name 'chatEnter' -Value $window.FindName('chatEnter') -Scope 1 -Force	
-New-Variable -Name 'chatLog' -Value $window.FindName('chatLog') -Scope 1 -Force	#Databind for text box
-	
+New-Variable -Name 'chatLog' -Value $window.FindName('chatLog') -Scope 1 -Force	
+New-Variable -Name 'buttonSelectPlayer' -Value $window.FindName('buttonSelectPlayer') -Scope 1 -Force	
+New-Variable -Name 'buttonSelectDM' -Value $window.FindName('buttonSelectDM') -Scope 1 -Force	#Databind for text box
+New-Variable -Name 'menuTab' -Value $window.FindName('menuTab') -Scope 1 -Force	#Databind for text box
+New-Variable -Name 'playerStats' -Value $window.FindName('playerStats') -Scope 1 -Force	#Databind for text box
+New-Variable -Name 'intro' -Value $window.FindName('intro') -Scope 1 -Force	#Databind for text box
 }
 
 function Load-Xaml {
@@ -16,9 +20,10 @@ function Load-Xaml {
 	$manager = New-Object System.Xml.XmlNamespaceManager -ArgumentList $xaml.NameTable
 	$manager.AddNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml");
 	$xaml.SelectNodes("//*[@x:Name='window']", $manager)[0].RemoveAttribute('Loaded')
-	$xaml.SelectNodes("//*[@x:Name='playerButton']", $manager)[0].RemoveAttribute('Click')
 	$xaml.SelectNodes("//*[@x:Name='playerPicture']", $manager)[0].RemoveAttribute('Click')
 	$xaml.SelectNodes("//*[@x:Name='chatEnter']", $manager)[0].RemoveAttribute('KeyDown')
+	$xaml.SelectNodes("//*[@x:Name='buttonSelectPlayer']", $manager)[0].RemoveAttribute('Click')
+	$xaml.SelectNodes("//*[@x:Name='buttonSelectDM']", $manager)[0].RemoveAttribute('Click')
 	$xamlReader = New-Object System.Xml.XmlNodeReader $xaml
 	[Windows.Markup.XamlReader]::Load($xamlReader)
 }
@@ -39,61 +44,32 @@ function Set-EventHandlers {
 		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
 		window_Loaded($sender,$e)
 	})
-	$playerButton.add_Click({
-		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
-		playerButton_Click($sender,$e)
-	})
 	$chatEnter.add_KeyDown({
 		param([System.Object]$sender,[System.Windows.Input.KeyEventArgs]$e)
 		chatEnter_KeyDown($sender,$e)
 	})
+	$buttonSelectPlayer.add_Click({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		buttonSelectPlayer_Click($sender,$e)
+	})
+	$buttonSelectDM.add_Click({
+		param([System.Object]$sender,[System.Windows.RoutedEventArgs]$e)
+		buttonSelectDM_Click($sender,$e)
+	})
 
 
 }
+
+
+
 
 $window = Load-Xaml
 Add-ControlVariables
 Set-EventHandlers
 
-function window_Loaded
-{
-	param($sender, $e)
-	$playerImg = playerImg1
-	$bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
-	$bitmap.BeginInit()
-	$bitmap.StreamSource = [System.IO.MemoryStream][System.Convert]::FromBase64String($playerImg)
-	$bitmap.EndInit()
-	$bitmap.Freeze()
-	$playerPicture.Source = $bitmap
-}
-
-#.(Join-Path $PSScriptRoot "modules.psm1")
-
-function submitButton_Click
-{
-	param($sender, $e)
-	msg console $firstNameText.text
 
 
-}
 
-function playerButton_Click
-{
-	param($sender, $e)
-
-}
-
-function chatEnter_KeyDown
-{
-	param($sender, $e)
-
-		if($sender.key -eq 'Return'){
-			$chatlog.AppendText($chatEnter.text)
-			$chatlog.AppendText($boxbindingtest)
-
-	}
-
-}
 
 $boxbindingtest ="Chat log goes here!
 Type in your message, or the message someone sent you!
@@ -105,6 +81,127 @@ I'm open to a better way to do this."
 Import-Module "C:\git\dndToolkit\powershell\data\resources.psm1"
 
 
+#Carry over from dndToolkit
+#region Pathing
+$workingDir = Split-Path -Parent $MyInvocation.MyCommand.path #$dndTranslatorPOSH_DMScreen_Load.file
+$dataDir = Split-Path -Parent $workingDir
+$dataDir = Split-Path -Parent $dataDir
+#endregion Pathing
+
+Import-Module  "$psscriptroot\dndTranslatorPOSH\dndTranslatorPOSH.psd1" -Force -Verbose
+
+$global:data = @{ } #share data between scopes
+
+# Language import
+$langMap = Get-Content "$dataDir\data\languages\default.json" -Raw
+$langMap = $langMap | ConvertFrom-Json
+$global:data.Add('langmap', $langMap)
+
+# Setup Players - not used for player screen
+#$players = Get-childitem C:\git\dndTranslator\data\playerFiles\ | ForEach-Object {Get-Content $_.FullName -Raw | ConvertFrom-Json}   #will need to set this up for proper pathing
+# Setup NPC
+#$npcs = Get-childitem C:\git\dndTranslator\data\npcFiles\ | ForEach-Object {Get-Content $_.FullName -Raw | ConvertFrom-Json}   #will need to set this up for proper pathing
+#$global:data.Add('players',$players) # not used for player screen
+$global:data.Add('npcs', $npcs)
+
+
+
+function window_Loaded
+{
+	param($sender, $e)
+	$playerImg = playerImg1
+	$bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
+	$bitmap.BeginInit()
+	$bitmap.StreamSource = [System.IO.MemoryStream][System.Convert]::FromBase64String($playerImg)
+	$bitmap.EndInit()
+	$bitmap.Freeze()
+	$playerPicture.Source = $bitmap
+}
+
+
+function chatEnter_KeyDown
+{
+	param($sender, $e)
+
+		if($sender.key -eq 'Return'){
+			$chatlog.AppendText($chatEnter.text)
+			$chatlog.AppendText($boxbindingtest)
+
+
+			# Need to add logic to detmine if they sent or receieved... Probably in Set-TranslateMessage tag with <SEND> if it sees <SEND> then strip it adn run get-translatedmessage.
+			<#
+	$translatedText = Set-TranslatedMessage -LanguageFile $global:data.langMap -Language $comboBox1.Text -Message $richTextBox2.Text
+	$nl = [Environment]::NewLine
+	$richtextbox1.AppendText((  $nl + "[ SEND ]" + $translatedText))
+	[System.Windows.Forms.Clipboard]::SetText($translatedText)
+
+	$translatedText = Get-TranslatedMessageAuto -LanguageFile $global:data.langMap  -Message $richTextBox4.Text -isDM $true
+	$nl = [Environment]::NewLine
+	$richtextbox1.AppendText((  $nl + "[ RECEIVE ]" + $translatedText))
+	$richtextbox4.Clear()
+
+			#>
+
+	}
+
+}
+
+function buttonSelectPlayer_Click
+{
+	param($sender, $e)
+	#
+	# Select the player file
+	#
+
+	[System.Windows.Forms.OpenFileDialog]$openFileDialog1 = $null
+	$openFileDialog1 = (New-Object -TypeName System.Windows.Forms.OpenFileDialog)
+	$openFileDialog1.ShowDialog()
+
+
+		# Read each file, import and convert from json into usable data
+
+	#                            $players = Get-childitem $playerFile | ForEach-Object { Get-Content $_.FullName -Raw | ConvertFrom-Json }
+
+
+	# Update databinging... update ui
+	#written for winforms              New-PlayerInfo -PlayersInfo $players -TargetPanel $flowLayoutPanel1
+
+
+
+	$playerStats.IsEnabled = $true
+	$intro.isEnabled = $false
+	$menuTab.selectedItem = $playerStats
+}
+
+
+
+
+function buttonSelectDM_Click
+{
+	param($sender, $e)
+
+	# Select the folder containing player files
+
+	[System.Windows.Forms.FolderBrowserDialog]$folderBrowserDialog1 = $null
+	$folderBrowserDialog1 = (New-Object -TypeName System.Windows.Forms.FolderBrowserDialog)
+	$folderBrowserDialog1.ShowDialog()
+
+
+
+	# Read each file, import and convert from json into usable data
+
+	#                            $players = Get-childitem $playerFile | ForEach-Object { Get-Content $_.FullName -Raw | ConvertFrom-Json }
+
+
+	# Update databinging... update ui
+	#written for winforms              New-PlayerInfo -PlayersInfo $players -TargetPanel $flowLayoutPanel1
+
+	#change tab
+	$intro.isEnabled = $false
+	$playerStats.IsEnabled = $true
+	$menuTab.selectedItem = $playerStats
+
+}
 
 
 $window.ShowDialog()
